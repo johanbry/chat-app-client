2dca3033552c04ae82b87f1cd6410eff8f88b732
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-08-16 17:13:51 +0200

    Routes to HomePage and ChatPage
    Co-authored-by: Therese  <thereselinden@users.noreply.github.com>
    Co-authored-by: Lokacitron123 <Lokacitron123@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
+
 type Props = {};
 
 const App = (props: Props) => {
-  return <div>App</div>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
