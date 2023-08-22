30c3501e1d8e7b306662803e53fef125ab45ce30
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-08-22 09:12:32 +0200

    Disconnect button and logics

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,12 +1,17 @@
 import RoomContainer from "../components/RoomContainer";
 import MessageContainer from "../components/MessageContainer";
+import Button from "../components/Button";
+import { useChatContext } from "../context/ChatContext";
 
 type Props = {};
 
 const ChatPage = (props: Props) => {
+  const { disconnectUser } = useChatContext();
+
   return (
     <>
       <h1>ChatPage</h1>
+      <Button text={"Sign out"} disabled={false} onClick={disconnectUser} />
       <RoomContainer />
 
       <MessageContainer />
