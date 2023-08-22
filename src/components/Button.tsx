4f619a9b4f1041fc265534b1851633120cb8636f
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-08-22 16:02:50 +0200

    refactored code reusable components and styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,34 @@
+import { ElementType } from "react";
+import "./button.scss";
+
 type Props = {
-  text: string;
+  text?: string;
+  Icon?: ElementType;
   disabled?: boolean;
   onChange?: () => void;
+  onClick?: () => void;
   type?: "submit" | "button" | "reset";
 };
 
 const Button = ({
   text,
+  Icon,
   disabled = true,
   onChange,
+  onClick,
   type = "submit",
 }: Props) => {
   return (
-    <button disabled={disabled} onChange={onChange} type={type}>
+    <button
+      disabled={disabled}
+      onChange={onChange}
+      type={type}
+      onClick={onClick}
+      className={Icon ? "icon-btn" : ""}
+      //className={disabled ? "disabled" : ""}
+    >
       {text}
+      {Icon && <Icon />}
     </button>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,27 +1,33 @@
-import { ChangeEvent } from "react";
-
+import { ChangeEvent, KeyboardEvent } from "react";
+import "./inputfield.scss";
 type Props = {
   type: string;
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
   placeholder?: string;
   required: boolean;
+  className?: string;
 };
 
 const InputField = ({
   type,
   value,
   onChange,
+  onKeyDown,
   required,
   placeholder,
+  className,
 }: Props) => {
   return (
     <>
       <input
+        className={className}
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        onKeyDown={onKeyDown}
         required={required}
       />
     </>
