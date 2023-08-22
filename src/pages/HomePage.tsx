89bef1138a4d1a5c473b8b51072fb29cd0699219
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-08-22 15:58:16 +0200

    styling and mobile check logic
    Co-authored-by: Lokacitron123 <Lokacitron123@users.noreply.github.com>
    Co-authored-by: Therese  <thereselinden@users.noreply.github.com>

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,17 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent, useEffect } from "react";
+import { HiChatBubbleLeftRight } from "react-icons/hi2";
+import { BiLogInCircle } from "react-icons/bi";
 
 import { useChatContext } from "../context/ChatContext";
 import InputField from "../components/InputField";
 import Button from "../components/Button";
 
+import "./homepage.scss";
+
 type Props = {};
 
 const HomePage = (props: Props) => {
-  const { connectUser } = useChatContext();
+  const { connectUser, isMobile } = useChatContext();
   const [username, setUsername] = useState("");
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
@@ -23,25 +27,33 @@ const HomePage = (props: Props) => {
   };
 
   return (
-    <>
-      <div>
-        <h1>Chat App</h1>
-      </div>
-      <div>
+    <div className="homepage-wrapper">
+      <section className="homepage-logo-container">
+        <HiChatBubbleLeftRight />
+        <h1>ChatApp</h1>
+      </section>
+      <span className="divider"></span>
+      <section className="homepage-form-container">
         <form onSubmit={handleConnect}>
-          <label htmlFor="username">Please fill in username</label>
+          {/* <label htmlFor="username">Please fill in username</label> */}
           <InputField
             type="text"
             placeholder="Enter username..."
             required
             value={username}
             onChange={handleInput}
+            className="signIn"
           />
+          {/* <Button text={"Connect"} disabled={!username} /> */}
 
-          <Button text={"Connect"} disabled={!username} />
+          {isMobile ? (
+            <Button text={"Connect"} disabled={!username} />
+          ) : (
+            <Button Icon={BiLogInCircle} disabled={!username} />
+          )}
         </form>
-      </div>
-    </>
+      </section>
+    </div>
   );
 };
 
