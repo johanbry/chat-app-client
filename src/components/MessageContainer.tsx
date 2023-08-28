fe7af8ed856fb8bb855edd128a7c5dca42bff50c
Therese Linden <theresemlinden@gmail.com>
2023-08-28 14:31:36 +0200

    Logic to show gifModal

--- src/components/MessageContainer.tsx
+++ src/components/MessageContainer.tsx
@@ -1,21 +1,24 @@
 import { ChangeEvent, FormEvent, useState, useEffect, useRef } from 'react';
 import { BsSend } from 'react-icons/bs';
+import { AiOutlineGif } from 'react-icons/ai';
+
 import { useChatContext } from '../context/ChatContext';
 import { socket } from '../socket';
-
 import Button from './Button';
 import InputField from './InputField';
 import ChatHeader from './ChatHeader';
 import MessageCard from './MessageCard';
-import { IUser } from '../context/ChatContext';
 
 import './messageContainer.scss';
 import { formatTypingUsers } from '../utils/helpers';
+import GifModal from './GifModal';
 
 type Props = {};
 
 const MessageContainer = (props: Props) => {
   const [message, setMessage] = useState<string>('');
+  const [showGifModal, setShowGifModal] = useState(false);
+
   const { messages, sendMessage, currentRoom, user, isMobile, typingUsers } =
     useChatContext();
   const chatWindow = useRef<HTMLDivElement>(null);
@@ -36,8 +39,6 @@ const MessageContainer = (props: Props) => {
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
-
-    // createRoom(createChatRoom);
   };
 
   const handleSendMessage = (e: FormEvent<HTMLFormElement>) => {
@@ -46,18 +47,26 @@ const MessageContainer = (props: Props) => {
     setMessage('');
   };
 
-  // const handleKeyDown = () => {
-  //   socket.emit("user_typing", user.username, currentRoom);
-  // };
-
-  //const chatWindow = useRef(null);
-
   useEffect(() => {
     if (chatWindow.current) {
       // Scroll to the latest message
       chatWindow.current.scrollTop = chatWindow.current.scrollHeight;
     }
   }, [messages]);
+
+  const handleToggleGif = () => {
+    setShowGifModal(!showGifModal);
+  };
+
+  const handleGifClick = (gif, e) => {
+    e.preventDefault();
+    console.log('GifClick', gif);
+    handleToggleGif();
+    setMessage(`/gif/${gif.id}`);
+
+    //! FUNGERAR EJ!!!!!!
+    sendMessage(message);
+  };
 
   return (
     <>
@@ -68,25 +77,32 @@ const MessageContainer = (props: Props) => {
         ))}
       </section>
 
-      {/* {typingUsers.length > 0 &&
-        typingUsers.map((user, index) => (
-          <p key={index}>{user.username} is typing...</p>
-        ))} */}
       {typingUsers.length > 0 && (
         <p className="typing-users">{formatTypingUsers(typingUsers)}</p>
+      )}
+      {showGifModal && (
+        <GifModal
+          handleToggleGif={handleToggleGif}
+          handleGifClick={handleGifClick}
+        />
       )}
       <form onSubmit={handleSendMessage} className="chat-message-form">
         <InputField
           type={'text'}
           value={message}
-          //onKeyDown={handleKeyDown}
-          // onKeyDown={handleUserTyping}
           onChange={handleInput}
           required
           placeholder="Enter message here..."
           className="no-border"
         />
 
+        <Button
+          Icon={AiOutlineGif}
+          disabled={false}
+          className="icon-btn icon-send-btn"
+          onClick={handleToggleGif}
+          type="button"
+        />
         <Button
           Icon={BsSend}
           disabled={!message}
