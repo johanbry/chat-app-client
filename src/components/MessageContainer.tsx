0a6c29de29d3c35ac60e5910097c1345fa0d3a06
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-08-29 15:12:27 +0200

    implement emojipicker, send giphy, improved typing

--- src/components/MessageContainer.tsx
+++ src/components/MessageContainer.tsx
@@ -1,35 +1,51 @@
-import { ChangeEvent, FormEvent, useState, useEffect, useRef } from 'react';
-import { BsSend } from 'react-icons/bs';
-import { AiOutlineGif } from 'react-icons/ai';
+import {
+  ChangeEvent,
+  FormEvent,
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+} from "react";
+import { BsSend } from "react-icons/bs";
+import { AiOutlineGif } from "react-icons/ai";
+import { BsEmojiSmile } from "react-icons/bs";
+import EmojiPicker from "emoji-picker-react";
+import { EmojiClickData } from "emoji-picker-react";
 
-import { useChatContext } from '../context/ChatContext';
-import { socket } from '../socket';
-import Button from './Button';
-import InputField from './InputField';
-import ChatHeader from './ChatHeader';
-import MessageCard from './MessageCard';
+import { useChatContext } from "../context/ChatContext";
+import { socket } from "../socket";
+import Button from "./Button";
+import InputField from "./InputField";
+import ChatHeader from "./ChatHeader";
+import MessageCard from "./MessageCard";
 
-import './messageContainer.scss';
-import { formatTypingUsers } from '../utils/helpers';
-import GifModal from './GifModal';
+import "./messageContainer.scss";
+import { formatTypingUsers } from "../utils/helpers";
+import GifModal from "./GifModal";
 
 type Props = {};
 
 const MessageContainer = (props: Props) => {
-  const [message, setMessage] = useState<string>('');
+  const [message, setMessage] = useState<string>("");
+  const [isTyping, setIsTyping] = useState(false);
   const [showGifModal, setShowGifModal] = useState(false);
+  const [showEmojiModal, setShowEmojiModal] = useState(false);
 
   const { messages, sendMessage, currentRoom, user, isMobile, typingUsers } =
     useChatContext();
   const chatWindow = useRef<HTMLDivElement>(null);
+  const bottomElement = useRef<HTMLDivElement>(null);
 
   //!FIX, skicka bara start en gång (lokalt isTyping-state?)
   useEffect(() => {
-    if (message) {
-      socket.emit('user_typing_start', user?.username, currentRoom);
+    if (message && !isTyping) {
+      // Om istping=false, första knapptrycket, skicka start, annars har vi redan skickat start
+      socket.emit("user_typing_start", user?.username, currentRoom);
+      setIsTyping(true);
     }
     const timer = setTimeout(() => {
-      socket.emit('user_typing_stop', currentRoom);
+      socket.emit("user_typing_stop", currentRoom);
+      setIsTyping(false);
     }, 3000);
 
     return () => {
@@ -44,28 +60,49 @@ const MessageContainer = (props: Props) => {
   const handleSendMessage = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     sendMessage(message);
-    setMessage('');
+    setMessage("");
+    socket.emit("user_typing_stop", currentRoom);
   };
 
   useEffect(() => {
+    // bottomElement?.current?.scrollIntoView({
+    //   behavior: "smooth",
+    //   block: "end",
+    //   inline: "end",
+    // });
     if (chatWindow.current) {
-      // Scroll to the latest message
-      chatWindow.current.scrollTop = chatWindow.current.scrollHeight;
+      //Scroll to the latest message
+      console.log("scrollheigt", chatWindow.current.scrollHeight);
+      console.log("scrollheigt+300", chatWindow.current.scrollHeight + 300);
+
+      chatWindow.current.scrollTop = chatWindow.current.scrollHeight + 300;
     }
   }, [messages]);
 
   const handleToggleGif = () => {
     setShowGifModal(!showGifModal);
+    if (showEmojiModal) handleToggleEmoji();
+  };
+
+  const handleToggleEmoji = () => {
+    setShowEmojiModal(!showEmojiModal);
+    if (showGifModal) handleToggleGif();
   };
 
   const handleGifClick = (gif, e) => {
     e.preventDefault();
-    console.log('GifClick', gif);
+    console.log("GifClick", gif);
     handleToggleGif();
-    setMessage(`/gif/${gif.id}`);
 
-    //! FUNGERAR EJ!!!!!!
-    sendMessage(message);
+    sendMessage(`/gif:${gif.id}`);
+  };
+
+  const handleEmojiClick = (emoji: EmojiClickData) => {
+    console.log(emoji);
+    setMessage((prev) => {
+      return prev + emoji.emoji;
+    });
+    handleToggleEmoji();
   };
 
   return (
@@ -75,27 +112,54 @@ const MessageContainer = (props: Props) => {
         {messages.map((message, index) => (
           <MessageCard key={index} message={message} />
         ))}
+        <div ref={bottomElement} style={{ height: 20 }}></div>
       </section>
 
       {typingUsers.length > 0 && (
-        <p className="typing-users">{formatTypingUsers(typingUsers)}</p>
+        <p className="typing-users">
+          {formatTypingUsers(typingUsers)}
+          <span className="bounching-container">
+            <span className="bounchingDots"></span>
+            <span className="bounchingDots"></span>
+            <span className="bounchingDots"></span>
+          </span>
+        </p>
       )}
+
       {showGifModal && (
         <GifModal
           handleToggleGif={handleToggleGif}
           handleGifClick={handleGifClick}
         />
       )}
+
+      {showEmojiModal && (
+        <div className="emoji-modal-wrapper">
+          <EmojiPicker
+            theme="dark"
+            previewConfig={{ showPreview: false }}
+            skinTonesDisabled={true}
+            onEmojiClick={handleEmojiClick}
+          />
+        </div>
+      )}
+
       <form onSubmit={handleSendMessage} className="chat-message-form">
         <InputField
-          type={'text'}
+          type={"text"}
           value={message}
           onChange={handleInput}
           required
           placeholder="Enter message here..."
           className="no-border"
         />
-
+        <Button
+          Icon={BsEmojiSmile}
+          disabled={false}
+          className="icon-btn icon-send-btn"
+          onClick={handleToggleEmoji}
+          type="button"
+        />
         <Button
           Icon={AiOutlineGif}
           disabled={false}
@@ -103,6 +167,7 @@ const MessageContainer = (props: Props) => {
           onClick={handleToggleGif}
           type="button"
         />
+
         <Button
           Icon={BsSend}
           disabled={!message}
