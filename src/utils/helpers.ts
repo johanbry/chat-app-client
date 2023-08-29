6920809e3b963315ca31a8830e3d40aaf5656fca
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-08-29 15:03:45 +0200

    changed typing string
    Co-authored-by: Therese  <thereselinden@users.noreply.github.com>
    Co-authored-by: Lokacitron123 <Lokacitron123@users.noreply.github.com>

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,18 +13,18 @@ export const formatTypingUsers = (users: IUser[]) => {
 
   switch (numOfUsers) {
     case 1:
-      str = `${users[0].username} is typing...`;
+      str = `${users[0].username} is typing`;
       break;
     case 2:
-      str = `${users[0].username} and ${users[1].username} are typing...`;
+      str = `${users[0].username} and ${users[1].username} are typing`;
       break;
     case 3:
-      str = `${users[0].username}, ${users[1].username} and ${users[2].username} are typing...`;
+      str = `${users[0].username}, ${users[1].username} and ${users[2].username} are typing`;
       break;
     default:
       str = `${users[0].username}, ${users[1].username}, ${
         users[2].username
-      } and ${numOfUsers - 3} others are typing...`;
+      } and ${numOfUsers - 3} others are typing`;
       break;
   }
 
