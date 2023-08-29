286c06c9e8aa68c0837cad38f0e88ae3640f9bd6
Johan Brynielsson <johan.brynielsson@gmail.com>
2023-08-29 15:09:10 +0200

    show giphy image in chat message and refactored

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,3 +1,8 @@
+import { useEffect, useRef, useState } from "react";
+import { Gif } from "@giphy/react-components";
+import { IGif } from "@giphy/js-types";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+
 import { IMessage, useChatContext } from "../context/ChatContext";
 import "./messageCard.scss";
 import { formatDate } from "../utils/helpers";
@@ -8,23 +13,56 @@ type Props = {
 
 const MessageCard = ({ message }: Props) => {
   const { user } = useChatContext();
+  const [gif, setGif] = useState<IGif | null>(null);
+
+  let messageType;
+
+  if (message.from === "system") messageType = "system";
+  else if (message.from === user?.username) messageType = "own";
+  else messageType = "other";
+
+  useEffect(() => {
+    const fetchGif = async () => {
+      const giphy = message.message.indexOf("/gif:");
+      if (giphy > -1) {
+        const id = message.message.substring(giphy + 5, message.message.length);
+        if (id) {
+          const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_API_KEY);
+          const { data } = await gf.gif(id);
+          setGif(data);
+        }
+      }
+    };
+    fetchGif();
+  }, [message.message]);
+
   return (
     <div className="chat-message-wrapper">
-      {message.from === "system" ? (
+      {messageType === "system" ? (
         <p className="system-message">{message.message}</p>
-      ) : message.from === user?.username ? (
-        <article className="user-message-container own-container">
-          <small>{message.from}</small>
-          <p className="user-message own">{message.message}</p>
-          <small>{formatDate(message.date)}</small>
-          <div className="clip-path-right"></div>
-        </article>
       ) : (
-        <article className="user-message-container other-container">
+        <article className={`user-message-container ${messageType}-container`}>
           <small>{message.from}</small>
-          <p className="user-message other">{message.message}</p>
+
+          {/* <p className={`user-message ${messageType}`}> */}
+          {gif ? (
+            <Gif
+              gif={gif}
+              width={300}
+              noLink
+              className={`user-message ${messageType}`}
+            />
+          ) : (
+            <p className={`user-message ${messageType}`}>{message.message}</p>
+          )}
+          {/* </p> */}
+
           <small>{formatDate(message.date)}</small>
-          <div className="clip-path-left"></div>
+          <div
+            className={`${
+              messageType === "own" ? "clip-path-right" : "clip-path-left"
+            }`}
+          ></div>
         </article>
       )}
     </div>
