1b069e8a6fdb3b760c95b1a495fee1b0903ca8e4
Therese Linden <theresemlinden@gmail.com>
2023-08-30 14:27:59 +0200

    Moved connect listener to useEffect. Removed unused code.

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -4,9 +4,9 @@ import {
   useContext,
   useEffect,
   useState,
-} from "react";
-import { useNavigate } from "react-router-dom";
-import { socket } from "../socket";
+} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { socket } from '../socket';
 
 interface IChatContext {
   isMobile: boolean;
@@ -52,7 +52,7 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [user, setUser] = useState<IUser | null>(null);
   const [messages, setMessages] = useState<IMessage[]>([]);
-  const [currentRoom, setCurrentRoom] = useState<string>("");
+  const [currentRoom, setCurrentRoom] = useState<string>('');
   const [rooms, setRooms] = useState<IRoom[] | null>(null);
   const [typingUsers, setTypingUsers] = useState<IUser[]>([]);
 
@@ -60,22 +60,22 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
 
   useEffect(() => {
     function updateTypingUsers(username: string, socketId: string) {
-      setTypingUsers((prev) => {
-        if (prev.some((user) => user.id === socketId)) return prev;
+      setTypingUsers(prev => {
+        if (prev.some(user => user.id === socketId)) return prev;
         return [...prev, { username, id: socketId }];
       });
     }
 
-    socket.on("send_typing_start", (username, socketId) =>
+    socket.on('send_typing_start', (username, socketId) =>
       updateTypingUsers(username, socketId)
     );
 
-    socket.on("send_typing_stop", (socketId) => {
-      setTypingUsers((prev) => prev.filter((user) => user.id !== socketId));
+    socket.on('send_typing_stop', socketId => {
+      setTypingUsers(prev => prev.filter(user => user.id !== socketId));
     });
 
     return () => {
-      socket.off("send_typing_start", updateTypingUsers);
+      socket.off('send_typing_start', updateTypingUsers);
     };
   }, []);
 
@@ -85,37 +85,31 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
       setIsMobile(window.innerWidth <= 767);
       setWindowWidth(window.innerWidth);
     };
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
 
     // Initial check
     handleResize();
     // Clean up the event listener on unmount
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   useEffect(() => {
-    socket.on("send_public_rooms", (rooms) => {
+    socket.on('send_public_rooms', rooms => {
       setRooms(rooms);
     });
   }, []);
 
   useEffect(() => {
-    socket.on("received_message", (message: IMessage) => {
-      // console.log("received message: ", message);
-
-      setMessages((prev) => [...prev, message]);
-    });
-
-    socket.io.on("reconnect", () => {
-      console.log("Reconnecting from client...");
+    socket.on('received_message', (message: IMessage) => {
+      setMessages(prev => [...prev, message]);
     });
   }, []);
 
   useEffect(() => {
     if (!user) {
-      navigate("/");
+      navigate('/');
     }
   }, []);
 
@@ -124,65 +118,66 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
       // Save in LS and pass room info to server
       // In LS because local state set back to default on mount = currentRoom is empty
       // Issue that we could not notify people in current that user left by (refresh/close window)
-      const lsRoom = localStorage.getItem("CurrentRoom");
-      socket.emit("leave_room", lsRoom);
-      socket.emit("user_typing_stop", lsRoom);
+      const lsRoom = localStorage.getItem('CurrentRoom');
+      socket.emit('leave_room', lsRoom);
+      socket.emit('user_typing_stop', lsRoom);
     };
 
-    window.addEventListener("beforeunload", handleWindowClose);
+    window.addEventListener('beforeunload', handleWindowClose);
 
     return () => {
-      window.removeEventListener("beforeunload", handleWindowClose);
+      window.removeEventListener('beforeunload', handleWindowClose);
     };
   }, []);
 
   useEffect(() => {
-    // if (!currentRoom) return;
-    localStorage.setItem("CurrentRoom", currentRoom);
+    localStorage.setItem('CurrentRoom', currentRoom);
+    // Since we dont want to show "user is typing" inside new room (if someone in prev room is typing when leaving)
+    setTypingUsers([]);
 
     return () => {
-      localStorage.removeItem("CurrentRoom");
+      localStorage.removeItem('CurrentRoom');
     };
   }, [currentRoom]);
 
   const connectUser = (username: string) => {
     if (!username) return;
     socket.connect();
-
-    //socket.emit("username_connected", username);
-
-    // kolla att connection är successfull
-    socket.on("connect", () => {
-      socket.emit("username_connected", username);
-      setCurrentRoom("Lobby");
-      setUser({ username, id: socket.id });
-      // console.log("userstate namn", user?.username);
-      // console.log("roomstate", currentRoom);
-      navigate("/chat");
-    });
+    setUser({ username, id: '' });
   };
 
+  useEffect(() => {
+    socket.on('connect', () => {
+      socket.emit('username_connected', user?.username);
+      setCurrentRoom('Lobby');
+      if (user) setUser({ ...user, id: socket.id });
+
+      navigate('/chat');
+    });
+    return () => {
+      socket.off('connect');
+    };
+  }, [user]);
+
   const disconnectUser = () => {
-    socket.emit("leave_room", currentRoom);
-
-    socket.disconnect();
-
     setUser(null);
     setMessages([]);
-    setCurrentRoom("");
+    setCurrentRoom('');
     setRooms([]);
-    navigate("/");
+    navigate('/');
+    socket.emit('leave_room', currentRoom);
+
+    socket.disconnect();
   };
 
   //!FIXME:
   const createRoom = (room: string) => {
     if (!room || currentRoom === room) return;
 
-    socket.emit("leave_room", currentRoom);
+    socket.emit('leave_room', currentRoom);
     setMessages([]);
     setCurrentRoom(room);
-    //if (room === currentRoom) return;
-    socket.emit("create_chatroom", room);
+    socket.emit('create_chatroom', room);
   };
 
   const sendMessage = (message: string) => {
@@ -191,11 +186,11 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
       message,
       currentRoom,
     };
-    socket.emit("message_from_client", msg);
+    socket.emit('message_from_client', msg);
   };
 
   const getUsersInRoom = (room: string): IUser[] | undefined => {
-    const currRoom = rooms?.find((element) => element.room === room);
+    const currRoom = rooms?.find(element => element.room === room);
     if (currRoom) return currRoom.users;
   };
 
