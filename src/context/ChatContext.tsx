e5069b69c77216182c59a72089fc1b29783c60ad
Therese Linden <theresemlinden@gmail.com>
2023-08-31 10:19:06 +0200

    Refactor code, added function comments
    Co-authored-by: Lokacitron123 <Lokacitron123@users.noreply.github.com>
    Co-authored-by: johanbry <johanbry@users.noreply.github.com>

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -6,44 +6,28 @@ import {
   useState,
 } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import { socket } from '../socket';
+import { IChatContext, IUser, IRoom, IMessage } from '../interfaces/interfaces';
 
-interface IChatContext {
-  isMobile: boolean;
-  windowWidth: number;
-  rooms: IRoom[] | null;
-  messages: IMessage[];
-  user: IUser | null;
-  currentRoom: string;
-  typingUsers: IUser[];
-  connectUser: (username: string) => void;
-  disconnectUser: () => void;
-  createRoom: (room: string) => void;
-  sendMessage: (message: string) => void;
-  getUsersInRoom: (room: string) => IUser[] | undefined;
-}
+const defaultValues = {
+  isMobile: true,
+  windowWidth: window.innerWidth,
+  rooms: null,
+  messages: [],
+  user: null,
+  currentRoom: 'Lobby',
+  typingUsers: [],
+  connectUser: () => {},
+  disconnectUser: () => {},
+  createJoinRoom: () => {},
+  sendMessage: () => {},
+  getUsersInRoom: () => {
+    return undefined;
+  },
+};
 
-export interface IRoom {
-  room: string;
-  users: IUser[];
-}
-
-export interface IUser {
-  username: string;
-  id: string;
-}
-
-export interface IMessage {
-  date: Date;
-  from: string;
-  message: string;
-}
-
-// const defaultValues = {
-
-// }
-
-export const ChatContext = createContext<IChatContext>(null as any);
+export const ChatContext = createContext<IChatContext>(defaultValues);
 
 export const useChatContext = () => useContext(ChatContext);
 
@@ -58,66 +42,72 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
 
   const navigate = useNavigate();
 
+  // Hanterar sockets som skriver meddelande i specifika rummet uppdaterar lokalt state med dessa sockets
   useEffect(() => {
+    // Uppdaterar listan med de användare som skriver i aktuellt rum just nu (om användare redan finns, gör ingenting.)
     function updateTypingUsers(username: string, socketId: string) {
       setTypingUsers(prev => {
         if (prev.some(user => user.id === socketId)) return prev;
         return [...prev, { username, id: socketId }];
       });
     }
-
+    // Lyssnar på event från server när användare börjar skriva
     socket.on('send_typing_start', (username, socketId) =>
       updateTypingUsers(username, socketId)
     );
-
+    // Lyssnar på event från server när användare slutar skriva och tar bort användaren ur typingUsers listan (arrayen)
     socket.on('send_typing_stop', socketId => {
       setTypingUsers(prev => prev.filter(user => user.id !== socketId));
     });
 
+    // Avregistrerar lyssnaren i en cleanup funktion. Och endast lyssnaren med denna specifika funktion på unmount
     return () => {
       socket.off('send_typing_start', updateTypingUsers);
     };
   }, []);
 
+  // Kollar storleken på skärmen och uppdaterar state utifrån skärmens bredd
+  // Beroende på state renderas antingen mobile eller desktop komponenter
   useEffect(() => {
     const handleResize = () => {
-      // Update isMobile based on the screen width
       setIsMobile(window.innerWidth <= 767);
       setWindowWidth(window.innerWidth);
     };
     window.addEventListener('resize', handleResize);
 
-    // Initial check
     handleResize();
-    // Clean up the event listener on unmount
+    // Avregistrerar lyssnaren i en cleanup funktion. Och endast lyssnaren med denna specifika funktion på unmount
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
 
+  // Lyssnar på eventet och uppdaterar lokalt state med antal aktiva rum. Aktiva rum är ej i socket memory, har egen struktur.
   useEffect(() => {
     socket.on('send_public_rooms', rooms => {
       setRooms(rooms);
     });
   }, []);
 
+  // Lyssnar på event och lägger till nya meddelande i lokala statet
   useEffect(() => {
     socket.on('received_message', (message: IMessage) => {
       setMessages(prev => [...prev, message]);
     });
   }, []);
 
+  // Navigera till root route om användar objekt är falskt
   useEffect(() => {
     if (!user) {
       navigate('/');
     }
-  }, []);
+  }, [user, navigate]);
 
+  // Sparar aktuellt rum i LS för att kunna meddela personer i rummet att användaren har lämnat (refresh / stänger fönster)
   useEffect(() => {
+    // Sparar rummet användaren som lämnar vid refresh / stänger fönster. Skickar event med info om rummet samt att användaren slutar skriva
+    // Sparar i LS pga state faller tillbaka på default vid omrendering = currentRoom blir då tomt
     const handleWindowClose = () => {
-      // Save in LS and pass room info to server
-      // In LS because local state set back to default on mount = currentRoom is empty
-      // Issue that we could not notify people in current that user left by (refresh/close window)
       const lsRoom = localStorage.getItem('CurrentRoom');
       socket.emit('leave_room', lsRoom);
       socket.emit('user_typing_stop', lsRoom);
@@ -130,9 +120,10 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
     };
   }, []);
 
+  // Uppdaterar LS när currentRoom state får nytt värde
   useEffect(() => {
     localStorage.setItem('CurrentRoom', currentRoom);
-    // Since we dont want to show "user is typing" inside new room (if someone in prev room is typing when leaving)
+    // Sätter tillbaka till defaultvärde. Pga visar "user is typing" inuti det nya rummet (Om någon skriver när vi lämnar)
     setTypingUsers([]);
 
     return () => {
@@ -140,12 +131,15 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
     };
   }, [currentRoom]);
 
+  // Eftersom vi har autoConnect: false hanterar vi connection med socket.connect()
+  // Kollar om användarnamn finns och sätter keyns value på user objektet.
   const connectUser = (username: string) => {
     if (!username) return;
     socket.connect();
     setUser({ username, id: '' });
   };
 
+  // Lyssnar på connect eventet. Skickar användarnamn till server och uppdaterar rum. Nu har vi fått tillbaka id och uppdaterar key value (id) på user objektet
   useEffect(() => {
     socket.on('connect', () => {
       socket.emit('username_connected', user?.username);
@@ -154,11 +148,14 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
 
       navigate('/chat');
     });
+
+    // Avregistrerar lyssnaren vid varje omrendrering
     return () => {
       socket.off('connect');
     };
-  }, [user]);
+  }, [user, navigate]);
 
+  // Tömmer state och skickar event och disconnectar
   const disconnectUser = () => {
     setUser(null);
     setMessages([]);
@@ -170,16 +167,19 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
     socket.disconnect();
   };
 
-  //!FIXME:
-  const createRoom = (room: string) => {
+  // Används till att skapa och ansluta sig till rum.
+  // Om rummet inte finns = skapar
+  // Socket skapar ett rum automatiskt om man försöker ansluta till ett rum som inte finns.
+  const createJoinRoom = (room: string) => {
     if (!room || currentRoom === room) return;
 
     socket.emit('leave_room', currentRoom);
     setMessages([]);
     setCurrentRoom(room);
-    socket.emit('create_chatroom', room);
+    socket.emit('create_join_chatroom', room);
   };
 
+  // Skapar ett meddelande objekt och skickar objektet som ett event till server
   const sendMessage = (message: string) => {
     const msg = {
       user,
@@ -189,6 +189,7 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
     socket.emit('message_from_client', msg);
   };
 
+  // Returnerar användarna som finns i det angivna rummet
   const getUsersInRoom = (room: string): IUser[] | undefined => {
     const currRoom = rooms?.find(element => element.room === room);
     if (currRoom) return currRoom.users;
@@ -202,7 +203,7 @@ const ChatProvider = ({ children }: PropsWithChildren) => {
         connectUser,
         disconnectUser,
         rooms,
-        createRoom,
+        createJoinRoom,
         messages,
         sendMessage,
         user,
